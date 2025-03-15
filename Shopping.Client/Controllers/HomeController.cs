a5bcef38f6fc06ec631c0433c0f649d08eb85279
vitaliip <vitalik11087625@gmail.com>
2025-03-15 23:31:49 +0100

    add project

--- Shopping.Client/Controllers/HomeController.cs
+++ Shopping.Client/Controllers/HomeController.cs
@@ -1,31 +1,32 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Shopping.Client.Data;
 using Shopping.Client.Models;
 
 namespace Shopping.Client.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+  private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
+  public HomeController(ILogger<HomeController> logger)
+  {
+    _logger = logger;
+  }
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+  public IActionResult Index()
+  {
+    return View(ProductContext.Products);
+  }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+  public IActionResult Privacy()
+  {
+    return View();
+  }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
+  [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+  public IActionResult Error()
+  {
+    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+  }
 }
